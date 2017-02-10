e526e45e48705e0961d0bf3015920003ed610faf
Jake Seaton <jakeseaton13@gmail.com>
2017-02-09 23:31:05 -0500

    fix blog views

--- blog/urls.py
+++ blog/urls.py
@@ -1,23 +1,20 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
-from ajax_select import urls as ajax_select_urls
+from django.views.generic import TemplateView
+import views
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', "blog.views.main"),
-    url(r'^post/(?P<slug>[a-zA-Z\d_\-]+)/$', 'blog.views.post'),
+
+    url(r'^$', views.main),
+    url(r'^(?P<category>archives|writing|art|multimedia)/$', views.main),
+
+    url(r'^post/(?P<slug>[a-zA-Z\d_\-]+)/$', views.post),
     # url(r'^theme', 'blog.views.theme'),
-    url(r'^tag/(?P<slug>[a-zA-Z\d_\-]+)/$', 'blog.views.tag_page'),
-    url(r'^theme/(?P<theme_id>[\d]+)$', 'blog.views.individual_theme'),
-    url(r'^contributor/(?P<author_id>[\d]+)$', 'blog.views.contributor_page'), 
-    url(r'^category/(?P<category_id>[\d]+)$', 'blog.views.view_category'),
-    url(r'^about', 'blog.views.about'),      
-    url(r'^archives/$|^writing/$|^art/$|^multimedia/$', 'blog.views.sections'),
+    url(r'^tag/(?P<slug>[a-zA-Z\d_\-]+)/$', views.tag_page),
+    url(r'^theme/(?P<theme_id>[\d]+)$', views.theme),
+    url(r'^contributor/(?P<author_id>[\d]+)$', views.contributor_page),
+    url(r'^about/$', TemplateView.as_view(template_name="blog_about.html")),
     url(r'^select2/', include('select2.urls')),
-    # url(r'^writing', 'blog.views.writing'),
-    # url(r'^multimedia', 'blog.views.multimedia'),
-    # url(r'^themes', 'blog.views.themes')
 )
--- blog/views.py
+++ blog/views.py
@@ -8,91 +8,91 @@ import json
 import stripe
 from django.conf import settings
 import random
+from django.shortcuts import render
 
 from .models import *
 
-def main(request):
-    """Main listing."""
-    posts = Post.objects.all()
-    all_posts_sorted = list(reversed(sorted(posts, key=lambda i: i.created)))
+BLOG_PATINATION_INTERVAL = 12
 
-    paginator = Paginator(all_posts_sorted, 12) # Show 25 contacts per page
-    page = request.GET.get('page')
+def main(request, category=None):
+    # get the page from the query string,
+    # default to 1 if none is provided
+    page = request.GET.get('page', 1)
+
+    posts = Post.objects.order_by("-created")
+
+    # if they provided a category
+    # art, writing, or multimedia
+    if category is not None:
+        # if its writing
+        if category == "writing":
+            # filter to any of the writing categories
+            categories = [
+                "Writing",
+                "fiction",
+                "Lyric essay",
+                "Review essay",
+                "Interview"
+            ]
+            posts = posts.filter(posted__name__in=categories)
+        # otherwise filter to the provided category
+        else:
+            posts = posts.filter(posted__name=category)
+
+    paginator = Paginator(posts, BLOG_PATINATION_INTERVAL)
+
     try:
-        blog_page = paginator.page(page)
+        current_page = paginator.page(page)
+
     except PageNotAnInteger:
         # If page is not an integer, deliver first page.
-        blog_page = paginator.page(1)
+        current_page = paginator.page(1)
+
     except EmptyPage:
         # If page is out of range (e.g. 9999), deliver last page of results.
-        blog_page = paginator.page(paginator.num_pages)
-    image_list = []
+        current_page = paginator.page(paginator.num_pages)
 
-    data = {
-        'posts': blog_page,
-        'posts_data': list(blog_page)
-    }    
-    template_name = 'blog.html'
-    return render_to_response(template_name, data, context_instance=RequestContext(request))
-    
+    context = {
+        'posts': current_page,
+        'name': category
+    }
+
+    return render(request, 'blog.html', context)
+
+
 def post(request, slug):
     post = get_object_or_404(Post, slug__iexact=slug)
-    data = {
-        'post': post
-    }
-    template_name = 'blog_post.html'
-    return render_to_response(template_name, data, context_instance=RequestContext(request))
-
-def about(request):
-    print 'GETTING ARTICLE'
-    template_name = 'blog_about.html'
-    return render_to_response(template_name, {}, context_instance=RequestContext(request))
+    return render(request, 'blog_post.html', {'post': post})
 
 
 def contributor_page(request, author_id):
-    this_author =  get_object_or_404(Author,id=author_id)
-    # author.name = name.replace("_", " ")
-    # author.id = author_id
-    data = {}
-    data["author"] = this_author.name
-    data["articles"] =  Post.objects.filter(authors__name=this_author.name)
-    template_name = 'blog_contributor.html'
-    return render_to_response(template_name, data, context_instance=RequestContext(request))
+    author = get_object_or_404(Author, id=author_id)
+    context = {
+        "author": author,
+        "articles": Post.objects.filter(authors=author)
+    }
+    return render(request, 'blog_contributor.html', context)
 
 
 def tag_page(request, slug):
-    this_tag =  get_object_or_404(Tag,slug__iexact=slug)
-    data = {}
-    data["author"] = this_tag.name
-    data["articles"] =  Post.objects.filter(tags__name=this_tag.name)
-    template_name = 'blog_tags.html'
-    return render_to_response(template_name, data, context_instance=RequestContext(request))
-
-
-def view_category(request, slug):
-    category = get_object_or_404(Category, slug=slug)
-    return render_to_response('blog_post.html', {
-        'category': category,
-        'posts': Post.objects.filter(category=category)
-    })
-
-
-def sections(request):
-    section = request.path
-    section = section.replace("/","").replace("blog","")
-
-    data = {
-        "name":section,
-        "issues": []
+    tag = get_object_or_404(Tag, slug__iexact=slug)
+    context = {
+        "tag": tag,
+        "articles": Post.objects.filter(tags=tag)
     }
-    if section == "writing":
-        posts_in_cat = Post.objects.filter(posted__name__in= ["Writing","fiction","Lyric essay","Review essay","Interview"]).distinct()
-    else:
-        posts_in_cat = Post.objects.filter(posted__name = section)
-    all_posts_sorted = list(reversed(sorted(posts_in_cat, key=lambda i: i.created)))
+    return render(request, 'blog_tags.html', context)
 
-    paginator = Paginator(all_posts_sorted, 12) # Show 25 contacts per page
-    page = request.GET.get('page')
+
+def theme(request):
+    section = request.path
+    section = section.replace("/", "").replace("blog", "")
+    page = request.GET.get('page', 1)
+
+    posts_in_cat = Post.objects.filter(theme__name=theme)
+    all_posts_sorted = posts_in_cat.order_by("-created")
+
+    paginator = Paginator(all_posts_sorted, BLOG_PATINATION_INTERVAL)
+
     try:
         blog_page = paginator.page(page)
     except PageNotAnInteger:
@@ -101,44 +101,9 @@ def sections(request):
     except EmptyPage:
         # If page is out of range (e.g. 9999), deliver last page of results.
         blog_page = paginator.page(paginator.num_pages)
-    image_list = []
 
     data = {
         'posts': blog_page,
-        'posts_data': list(blog_page),
         'name': section
-    }    
-    template_name = 'blog.html'
-    return render_to_response(template_name, data, context_instance=RequestContext(request))
-
-def individual_theme(request):
-    section = request.path
-    section = section.replace("/","").replace("blog","")
-
-    data = {
-        "name":section,
-        "issues": []
     }
-
-    posts_in_cat = Post.objects.filter(theme__name = theme)
-    all_posts_sorted = list(reversed(sorted(posts_in_cat, key=lambda i: i.created)))
-
-    paginator = Paginator(all_posts_sorted, 12) # Show 25 contacts per page
-    page = request.GET.get('page')
-    try:
-        blog_page = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        blog_page = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        blog_page = paginator.page(paginator.num_pages)
-    image_list = []
-
-    data = {
-        'posts': blog_page,
-        'posts_data': list(blog_page),
-        'name': section
-    }    
-    template_name = 'blog_section.html'
-    return render_to_response(template_name, data, context_instance=RequestContext(request))
+    return render(request, 'blog_section.html', data)
